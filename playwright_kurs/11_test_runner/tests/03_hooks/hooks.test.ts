91d81fdf6dc282bcef775aff90e87b84f11af47c
dpinezich <david.pinezich@gmail.com>
2024-12-06 17:35:45 +0100

    update

--- playwright_kurs/11_test_runner/tests/03_hooks/hooks.test.ts
+++ playwright_kurs/11_test_runner/tests/03_hooks/hooks.test.ts
@@ -1,15 +1,27 @@
-import { test, expect } from '@playwright/test';
+import {test} from '@playwright/test';
 
-test.describe('Feature Group', () => { 
+test.describe('Feature Group', () => {
 
-    test('Test 1', async ({ page }) => {
-        await page.goto('');
+    // a before all would be cool!
+
+    test.beforeEach('Description before', async ({page}) => {
+        console.log('Before each');
+    });
+
+    test('Test 1', async ({page}) => {
         console.log("Test 1");
     });
 
-    test('Test 2', async ({ page }) => {
-        await page.goto('');
+    test('Test 2', async ({page}) => {
         console.log("Test 2");
+    });
+
+    test.afterEach('Description after', async () => {
+        console.log('After each');
+    });
+
+    test.afterAll('Finally', async () => {
+        console.log('one more time..');
     });
 
 });
