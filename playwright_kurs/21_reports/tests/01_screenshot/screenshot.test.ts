050a66804d37b69623175e6dbc43bfd4cc2388d9
dpinezich <david.pinezich@gmail.com>
2024-12-06 13:02:48 +0100

    update

---
+++ playwright_kurs/21_reports/tests/01_screenshot/screenshot.test.ts
@@ -0,0 +1,18 @@
+import { test } from '@playwright/test';
+
+test('home page screenshot', async ({ page }, testInfo) => {
+    await page.goto('https://checklyhq.com');
+
+    const homeScreenshot = await page.screenshot();
+    testInfo.attach('Home Page', {
+        body: homeScreenshot,
+        contentType: 'image/png',
+    });
+
+    await page.click('nav a[href="/customers/"]');
+    const customersScreenshot = await page.screenshot();
+    testInfo.attach('Customers Page', {
+        body: customersScreenshot,
+        contentType: 'image/png',
+    });
+});
