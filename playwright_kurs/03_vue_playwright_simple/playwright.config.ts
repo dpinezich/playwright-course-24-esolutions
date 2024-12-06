818c8af6f5e73c93e7568e963790bceea4388b08
dpinezich <david.pinezich@gmail.com>
2024-12-06 17:44:09 +0100

    update

---
+++ playwright_kurs/03_vue_playwright_simple/playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  reporter: 'html',
+ 
+  use: {
+    baseURL: 'http://localhost:3099/',
+    headless: false,
+    launchOptions: { slowMo: 1000 },
+  },
+  
+
+  webServer: {
+    command: 'npm start',
+    url: 'http://localhost:3099/',
+  }
+});
